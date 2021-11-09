177f2727d545c06c790ed1abe73e35b2dc07a885
EduardoVega04 <eduarvega04@hotmail.com>
2021-11-09 11:42:16 -0500

    webpack

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -6,9 +6,9 @@ module.exports = {
     mode: "development",
     devtool: 'inline-source-map',
     entry: {
-        header: path.resolve(__dirname, './modules/header/header.js'),
-        body: path.resolve(__dirname, './modules/body/body.js'),
-        footer: path.resolve(__dirname, './modules/footer/footer.js'),
+        header: './modules/header/header.js',
+        body: './modules/body/body.js',
+        footer: './modules/footer/footer.js'
     },
     output: {
         filename: "[name].bundle.js",
