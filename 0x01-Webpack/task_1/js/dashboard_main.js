3f37270faa14b9d525664ca06c3c31914df9e5a5
EduardoVega04 <eduarvega04@hotmail.com>
2021-11-09 07:05:48 -0500

    Learning how to use Webpack with a config file

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -8,7 +8,7 @@ $(function () {
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<button type="button">Click here to get started</button>');
     $('body').append(`<p id='count'></p>`);
-    $('body').append('Copyright - Holberton School');
+    $('body').append('<p>Copyright - Holberton School</p>');
 
     function updateCounter() {
         count++;
