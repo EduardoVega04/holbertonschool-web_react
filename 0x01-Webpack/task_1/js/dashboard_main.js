0d7fe0c1088c35051f4b8702980f8deb8157e5c3
EduardoVega04 <eduarvega04@hotmail.com>
2021-11-09 08:01:08 -0500

    Learning how to use Webpack with a config file

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,20 +1,19 @@
 import $ from "jquery";
 import _ from "lodash";
 
-$(function () {
-    let count = 0;
 
-    function updateCounter() {
-        count++;
-        $("#count").text(`${count} clicks on the button`);
-    }
+let count = 0;
 
-    const my_button = $('<button>Click here to get started</button>').on(
-        "click", _.debounce(updateCounter, 500));
+function updateCounter() {
+    count++;
+    $("#count").text(`${count} clicks on the button`);
+}
 
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append(my_button);
-    $('body').append(`<p id='count'></p>`);
-    $('body').append('<p>Copyright - Holberton School</p>');
-});
+const my_button = $('<button>Click here to get started</button>').on(
+    "click", _.debounce(updateCounter, 500));
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append(my_button);
+$('body').append(`<p id='count'></p>`);
+$('body').append('<p>Copyright - Holberton School</p>');
