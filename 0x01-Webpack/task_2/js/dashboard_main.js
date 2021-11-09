363143c523560de3bb556f97211d7c432e43085b
EduardoVega04 <eduarvega04@hotmail.com>
2021-11-09 09:05:09 -0500

    Custom webpack config

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,5 +1,7 @@
 import $ from "jquery";
 import _ from "lodash";
+import '../css/main.css';
+import '../assets/holberton-logo.jpg';
 
 $(function () {
     let count = 0;
@@ -12,6 +14,7 @@ $(function () {
     const my_button = $('<button>Click here to get started</button>').on(
         "click", _.debounce(updateCounter, 500));
 
+    $("body").append('<div id="logo"></div>');
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append(my_button);
