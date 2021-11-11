9aadbde68c294b6ec9f4e92a75200e9d235e0110
EduardoVega04 <eduarvega04@hotmail.com>
2021-11-11 03:55:53 -0500

    Create React tests with enzyme

---
+++ 0x02-react_intro/task_3/dashboard/src/Notifications.test.js
@@ -0,0 +1,20 @@
+// npm run test
+import { shallow } from 'enzyme';
+import Notifications from './Notifications';
+
+describe('<Notifications />', () => {
+    it('Renders without crashing', () => {
+        const wrapper = shallow(<Notifications />);
+        expect(wrapper.exists()).toEqual(true);
+    });
+
+    it('Renders three list items (li)', () => {
+        const wrapper = shallow(<Notifications />);
+        expect(wrapper.find('ul').children().length).toEqual(3);
+    });
+
+    it('Renders the text "Here is the list of notifications"', () => {
+        const wrapper = shallow(<Notifications />);
+        expect(wrapper.find('p').render().text()).toEqual("Here is the list of notifications");
+    });
+});
--- 0x02-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x02-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,20 +1,28 @@
 // npm run test
 import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
 
-test('Get current year (number)', () => {
-    expect(getFullYear()).toBe(2021);
+describe('getFullYear()', () => {
+    it('Returns the current year as a number', () => {
+        expect(getFullYear()).toBe(2021);
+    });
 });
 
-test('Get correct string according parameter (true)', () => {
-    expect(getFooterCopy(true)).toBe("Holberton School");
+describe('getFooterCopy()', () => {
+    it('Get correct string according parameter (true)', () => {
+        expect(getFooterCopy(true)).toBe("Holberton School");
+    });
 });
 
-test('Get correct string according parameter (false)', () => {
-    expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+describe('getFooterCopy()', () => {
+    it('Get correct string according parameter (false)', () => {
+        expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+    });
 });
 
-test('Check returned string', () => {
-    expect(getLatestNotification().__html).toBe(
-        '<strong>Urgent requirement</strong> - complete by EOD'
-    );
+describe('getLatestNotification()', () => {
+    it('Check returned string', () => {
+        expect(getLatestNotification().__html).toBe(
+            '<strong>Urgent requirement</strong> - complete by EOD'
+        );
+    });
 });
