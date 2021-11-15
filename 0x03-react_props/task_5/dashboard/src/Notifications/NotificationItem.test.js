37e44042fb7258ee168fd80d431bedc4aefeb2b9
EduardoVega04 <eduarvega04@hotmail.com>
2021-11-15 08:47:41 -0500

    Shapes, Loops, and Keys

---
+++ 0x03-react_props/task_5/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,32 @@
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom'
+import NotificationItem from './NotificationItem';
+import { getLatestNotification } from '../utils/utils';
+import React from 'react';
+
+describe('<NotificationItem />', () => {
+    it('Renders the first component without crashing', () => {
+        const wrapper = shallow(<NotificationItem type="default" value="New course available" />);
+        expect(wrapper.exists()).toEqual(true);
+    });
+
+    it('Renders the second component without crashing', () => {
+        const wrapper = shallow(<NotificationItem type="urgent" value="New resume available" />);
+        expect(wrapper.exists()).toEqual(true);
+    });
+
+    it('Renders the third component without crashing', () => {
+        const wrapper = shallow(<NotificationItem type="urgent" html={getLatestNotification()} />);
+        expect(wrapper.exists()).toEqual(true);
+    });
+
+    it('Verify that by passing dummy type and value props, it renders the correct html', () => {
+        const wrapper = shallow(<NotificationItem type="default" value="test" />);
+        expect(wrapper.render().text()).toEqual("test");
+    });
+
+    it('Verify that by passing a dummy html prop, it renders the correct html', () => {
+        const wrapper = shallow(<NotificationItem type="default" html={{ __html: '<u>test</u>' }} />);
+        expect(wrapper.html()).toEqual('<li data-notification-type="default"><u>test</u></li>');
+    });
+});
