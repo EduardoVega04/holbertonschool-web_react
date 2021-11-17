aa4dd4343d8ab47fa0a1893f5d081b1173f2df76
EduardoVega04 <eduarvega04@hotmail.com>
2021-11-17 00:22:11 -0500

    Convert function componnt to class

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -10,6 +10,10 @@ import { getLatestNotification } from '../utils/utils';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render () {
     return (
       <React.Fragment>
